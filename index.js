af62e7145d2d658be21da3bad2090010d78f00ab
Vladislav Dontsov <dontsov.vladislav@gmail.com>
2020-11-27 10:53:23 +0200

    added config

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const config = require('./config');
 const db = require('./models');
 const userRoute = require('./routes/user');
 const  cors = require('cors')
